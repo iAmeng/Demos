2f4c54ae33dee8873bb020329aa3f0e2d14ba293
iameng <developerwolf@163.com>
2016-03-31 15:41:38 +0800

    加了个java main

---
+++ edittextpropterty/src/main/java/imeng/edittextpropterty/Mainjava.java
@@ -0,0 +1,13 @@
+package imeng.edittextpropterty;
+
+/**
+ * @project: DDZH
+ * @Author : Administrator
+ * @Date : 2016/3/31 15:38
+ * @Version:
+ */
+public class Mainjava {
+    public static void main(String arg[]) {
+        System.out.println("hello world");
+    }
+}
