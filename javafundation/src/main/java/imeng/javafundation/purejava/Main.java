e69d9fdd684dc8a06bbdd2211d8285b0e4f73d1f
iameng <developerwolf@163.com>
2016-04-01 11:30:28 +0800

    java 语法基础 main

---
+++ javafundation/src/main/java/imeng/javafundation/purejava/Main.java
@@ -0,0 +1,14 @@
+package imeng.javafundation.purejava;
+
+/**
+ * @project: DDZH
+ * @Author : Administrator
+ * @Date : 2016/4/1 11:29
+ * @Version:
+ */
+public class Main {
+    public static void main(String args[]) {
+        System.out.println("Hello Wrold");
+    }
+}
+
